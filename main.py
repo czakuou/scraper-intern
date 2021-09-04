76600fdacdd110b0367aabda4ee0cf8320dcfd7c
czakuou <emil.krzakuou@gmail.com>
2021-09-04 15:05:28 +0200

    fix csv header bug

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from typing import Iterator
+from typing import Iterator, Sequence, TypeVar
 from requests import get
 from bs4 import BeautifulSoup
 import time
 import os
+import csv
 
 HEADERS = {
     "User-Agent":
@@ -10,18 +11,53 @@ HEADERS = {
 }
 LINK = 'www.searchenginejournal.com'
 
-FILE_NAME = ['url_results.csv', 'keyword_counter.txt']
+FILE_NAME = ['url_results.csv', 'keyword_counter.csv']
+
+T = TypeVar('T', list, int)
 
 
-def create_files():
-    for name in FILE_NAME:
-        if not os.path.isfile(name):
-            with open(name, 'w') as csv:
-                pass
+def create_url_file() -> None:
+    """Create url_results.csv file if not exists"""
+    if not os.path.isfile(FILE_NAME[0]):
+        with open(FILE_NAME[0], 'a', newline='') as csvfile:
+            fieldnames = ['keyword', 'URL']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
+            writer.writeheader()
+
+
+def create_count_file() -> None:
+    """Create keyword_counter.csv file if not exists"""
+    if not os.path.isfile(FILE_NAME[1]):
+        with open(FILE_NAME[1], 'a', newline='') as csvfile:
+            fieldnames = ['keyword', 'count']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+
+
+def file_writer(data: Sequence[T], keyword: str) -> None:
+    """
+    Write data to files
+    The sequence have to be in order
+    (URL, counter)
+    """
+    with open(FILE_NAME[0], 'a', newline='') as csvfile:
+        fieldnames = ['keyword', 'URL']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        writer.writerow({fieldnames[0]: keyword, fieldnames[1]: data[0]})
+
+    with open(FILE_NAME[1], 'a', newline='') as csvfile:
+        fieldnames = ['keyword', 'count']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        writer.writerow({fieldnames[0]: keyword, fieldnames[1]: data[1]})
 
 
 def read_file(path: str) -> Iterator[str]:
+    """Keywords loader"""
     with open(path) as f:
         for row in f:
             yield row.strip()
@@ -33,9 +69,6 @@ def read_file(path: str) -> Iterator[str]:
     #         keywords_list[i] = row
     #
     # return keywords_list
-
-
-
 
 
 def scraper(keyword: str):
@@ -54,17 +87,17 @@ def scraper(keyword: str):
     # links = [link.a['href'] for link in bs.find_all('div', class_='yuRUbf')
     #          if link.a['href'].split('/')[2] == LINK]
     time.sleep(2)
-    return links
+    return (links, count)
 
 
 def main():
     create_files()
+    print('[INFO] Start wraper')
     for keyword in read_file('keywords.txt'):
-        print(scraper(keyword))
-        # scraper(keyword)
-        break
+        data = scraper(keyword)
+        file_writer(data, keyword)
+        print('[INFO] Saved URL for {}'.format(keyword))
 
 
 if __name__ == '__main__':
     main()
-
